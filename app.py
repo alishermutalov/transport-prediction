05c05fbedcda8c15aeee9dedc6953cd7e1aede1e
alishermutalov <96579968+alishermutalov@users.noreply.github.com>
2025-02-22 20:29:18 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from fastai.vision.all import *
+import plotly.express as px
+import pathlib
+
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+
+st.title('Transport classification model')
+file = st.file_uploader('Upload image')
+if file:
+    st.image(file)
+    img = PILImage.create(file)
+    model = load_learner('transport_model.pkl')
+
+    pred, pred_id, prob = model.predict(img)
+
+    def translate(text):
+        if pred=='Car':
+            return 'Avtomobil'
+        elif pred=='Boat':
+            return 'Suv transporti (qayiq, kema, etc.)'
+        elif pred=='Airplane':
+            return 'Samalyot'
+
+    st.success(f"Rasmda: {translate(pred)}")
+    st.info(f"Aniqlik: {prob[pred_id]*100:.1f}%")
+    
+    fig = px.bar(x=prob*100,y=model.dls.vocab)
+    st.plotly_chart(fig)
+else:
+    st.info("Rasm kititing!")
